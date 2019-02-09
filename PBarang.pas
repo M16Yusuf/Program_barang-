788eeb32362861d42eceeb352d3fce55b0b355f7
Muhammad Yusuf <yusufsmd58@gmail.com>
2019-02-09 18:57:05 +0700

    perubahan struktur data

--- PBarang.pas
+++ PBarang.pas
@@ -1,10 +1,26 @@
 program PBarang;
 uses crt;
 
-type
-    
-var 
-    array
-begin
+const 
+    maks=100;
+    namaFile = 'dataBar.dat';
 
+type
+    TBarang = record 
+        Kode_barang : string;
+        nama_barang : string;
+        harga : real;
+        subtotal: real;
+        Qty : integer;
+
+
+var
+    surat : array[1..maks] of TBarang;
+    banyakdata :integer;
+    pilihan_menu : integer;
+
+begin
+    writeln('apa aja weeh');
+    writeln('apa ajaa weeeh');
+    writeln('aapaa atuuh');    
 end.
